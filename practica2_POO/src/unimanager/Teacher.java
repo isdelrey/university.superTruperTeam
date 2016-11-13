f96118493ecf0f626a4d2474fb42d2f036fbd75e
bassagap <bassagap@RTCMW0859M.emea.roche.com>
2016-11-13 13:24:18 +0100

    [13.11.2016] bassagap: Adding methods and attributes to Teacher class, so that it fullfils the relationships specifications.

--- practica2_POO/src/unimanager/Teacher.java
+++ practica2_POO/src/unimanager/Teacher.java
@@ -8,14 +8,35 @@ Ivo Sequeros del Rey, NIA: 183711
 
  */
 package unimanager;
-
+import java.util.LinkedList;
 /**
  *
  * @author ivo
  */
 public class Teacher {
-    String name;
+    
+    // Attributes: 
+    private String name;
+    private LinkedList<Assignment> assignments;
+    
+    // Contructor:
     public Teacher(String NAME) {
         name = NAME;
     }
+    
+    //Public methods: 
+    public String getName(){
+      return this.name;  
+    };
+    public LinkedList<Assignment> assignments(){
+        return this.assignments;
+    };
+    
+    // Add and setter
+    public void setName(String n){
+        this.name = n;
+    };
+    public void addAssignment(Assignment a){
+        this.assignments.add(a);
+    };
 }
