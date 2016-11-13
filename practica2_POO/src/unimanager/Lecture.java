305a3a8436bcf9ea3b3ab55539e88dfac84e5254
ivosequeros <ivo@MacBook-Pro-de-Ivo-2.local>
2016-11-13 21:11:57 +0100

    Lecture Merge resolved

--- practica2_POO/src/unimanager/Lecture.java
+++ practica2_POO/src/unimanager/Lecture.java
@@ -45,20 +45,12 @@ public class Lecture {
     public int getType(){
         return this.type;
     };
-<<<<<<< HEAD
     public int getGeneralGroup(){
         return (this.group < 10)?this.group:((this.group < 100)?this.group/10:this.group/100);
     };
-=======
-<<<<<<< HEAD
-    
-    
-=======
->>>>>>> origin/master
     public Course getCourse(){
         return this.course;
     };
->>>>>>> origin/master
    //Adds and setters:
    public void addClassroom(Classroom c){
        this.classroom = c;
