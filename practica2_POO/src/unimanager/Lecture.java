e58fe7b5815e620a8467fc9aed6377051a5b470a
bassagap <bassagap@RTCMW0859M.emea.roche.com>
2016-11-13 12:57:19 +0100

    [13.11.2016] bassagap: Adding methods and attributes to Lecture class, so that it fullfils the relationships specifications.

--- practica2_POO/src/unimanager/Lecture.java
+++ practica2_POO/src/unimanager/Lecture.java
@@ -14,10 +14,48 @@ package unimanager;
  * @author ivo
  */
 public class Lecture {
-    String group;
-    int timeSlot;
-    int type;
+    
+    //Attributes
+    private String group;
+    private int timeSlot;
+    private int type;
+    private Classroom classroom;
+    private Course course;
+    
+    //Contructor:
     public Lecture(String GROUP, int TIMESLOT, int TYPE) {
-        
+        this.group = GROUP; 
+        this.timeSlot = TIMESLOT; 
+        this.type = TYPE;
     }
+    
+    // Public methods:
+    //getters
+    public String getGroup(){
+        return this.group;  
+    };
+    public int getTimeSlot(){
+        return this.timeSlot;
+    };
+    public int getType(){
+        return this.type;
+    };
+   //Adds and setters:
+   public void addClassroom(Classroom c){
+       this.classroom = c;
+   }; 
+   public void addCourse(Course c){
+       this.course = c;
+   };
+   public void setGroup( String g){
+       this.group = g;
+   };
+   public void setTimeSlot( int i){
+       this.timeSlot = i;
+   };
+   public void setType(int i){
+       this.type = i;
+   };
+   
+   
 }
