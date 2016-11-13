42fd2b10b75db730f4ebe71306b55ac618faee07
bassagap <bassagap@RTCMW0859M.emea.roche.com>
2016-11-13 12:03:23 +0100

    [13.11.2016] bassagap: Adding methods and attributes to Course class, so that it fullfils the relationships specifications.

--- practica2_POO/src/unimanager/Course.java
+++ practica2_POO/src/unimanager/Course.java
@@ -8,14 +8,54 @@ Ivo Sequeros del Rey, NIA: 183711
 
  */
 package unimanager;
-
+import java.util.LinkedList;
 /**
  *
  * @author ivo
  */
 public class Course {
-    String name;
+    private String name;
+    private LinkedList<Enrollment> enrollments; 
+    private LinkedList<Assignment> assignments;
+    private LinkedList<Lecture> lectures; 
+    
+    // Contructor:
     public Course(String NAME) {
         name = NAME;
     }
+    
+    // Public methods:
+    //getters
+    public LinkedList<Enrollment> getEnrollments(){
+      return this.enrollments;  
+    };
+    public LinkedList<Assignment>  getAssignments(){
+        return this.assignments; 
+    };
+    public LinkedList<Lecture> getLectures(){
+        return this.lectures; 
+    };
+    
+    //setters
+    public void setEnrollments(LinkedList<Enrollment> enrollments){
+        this.enrollments = enrollments; 
+    };
+    public void setAssignments(LinkedList<Assignment> assignments){
+        this.assignments = assignments;
+    }
+    public void setLectures(LinkedList<Lecture> lectures){
+        this.lectures = lectures;
+    };
+    
+    //Add 1 enrollment, assignment or lecture to the current ones.
+    public void addEnrollments(Enrollment e){
+       this.enrollments.add(e); 
+    };
+    public void addAssignments(Assignment a){
+      this.assignments.add(a);  
+    };
+    public void addLecture(Lecture l){
+      this.lectures.add(l);
+    };
+   
 }
