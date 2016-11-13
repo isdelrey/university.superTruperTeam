ae40bb282c20c53dc38e316cf06a24745bed5231
bassagap <bassagap@RTCMW0859M.emea.roche.com>
2016-11-13 13:09:48 +0100

    [13.11.2016] bassagap: Adding methods and attributes to Student class, so that it fullfils the relationships specifications.

--- practica2_POO/src/unimanager/Student.java
+++ practica2_POO/src/unimanager/Student.java
@@ -8,16 +8,45 @@ Ivo Sequeros del Rey, NIA: 183711
 
  */
 package unimanager;
+import java.util.LinkedList;
 
 /**
  *
  * @author ivo
  */
 public class Student {
+    
+    // Attributes: 
     String name;
     int nia;
+    LinkedList<Enrollment> enrollments; 
+    
+    //Contructor:
     public Student(String NAME,int NIA) {
         nia = NIA;
         name = NAME;
     }
+    
+    // Public methods: 
+    //getters
+    public String getName(){
+        return this.name;
+    };
+    public int getNia(){
+        return this.nia;
+    }; 
+    public LinkedList<Enrollment> getEnrollments(){
+        return this.enrollments;
+    };
+    
+    //Adds and setters:
+    public void setName(String n){
+        this.name = n;
+    };
+    public void setNia(int n){
+        this.nia = n;
+    };
+    public void addEnrollment(Enrollment e){
+        this.enrollments.add(e);
+    };
 }
