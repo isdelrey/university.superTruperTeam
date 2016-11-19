bd401b237bf569b4b359e11d2855a4ebca822c44
bassagap <bassagap@RTCMW0859M.emea.roche.com>
2016-11-19 09:30:42 +0100

    Changes on Matrix class
    1. Deleted constructor
    2. get columns and rows numbers (#rows & #cols)
    3. Changes in the concept: a matrix is an array of vectors, so it is
    built of columns vectors: [v1, v2, ... , vn] so each column is going to
    be a vector and each row is going to be a combination of vectors row1 =
    [v1[1], v2[1], v3[1], ... vn[1]]
    4. Printing a matrix
    5. 3D Rotation matrix

--- practica3_POO/src/editor/Matrix.java
+++ practica3_POO/src/editor/Matrix.java
@@ -10,35 +10,99 @@ package editor;
  * @author ivo
  */
 public class Matrix extends ObjectCollection {
+    
+    //Attributes
     private Vector[] values, transposed;
     private int rows, cols;
-    public Matrix() {
-        values = new Vector[maxDimension];
-    }
-    public Vector getRow(int r) {
-        return values[r];
-    }
-    public Vector getColumn(int c) {
-        Vector v = new Vector();
-        for(int i=0;i<rows;i++) v.set(i,values[i].get(c));
+    
+    //Constructor
+    public Matrix(Integer m, Integer n) {
+        this.cols = n;
+        this.rows = m;
+       this.values = new Vector[n];        
+        for(Integer i= 0; i<n; i++){
+            this.values[i] = new Vector(m);
+        }      
+    };
+    //Public methods:
+    //getters
+    public int getRowsNumber() {
+        return this.rows;
+    };
+    public int getColumnsNumber() {
+        return this.cols;
+    };
+    public Vector[] getMatrix (){
+        return this.values; 
+    };
+    
+    // Returns the column vector of the matrix belonging to column c
+    public Vector getColumnVector(int c) {
+        return this.values[c];
+    };
+    
+    // Returns the Row vector of the matrix belonging to row r
+    public Vector getRowVector(int r) {
+        Vector v = new Vector(this.cols);
+        for(int i=0;i<this.rows;i++) {
+            v.set(i,values[i].getPositionValue(r));
+        }
         return v;
-    }
-    public void setRow(int r, Vector v) {
-        values[r] = v;
-    }
+    };
+    
+    //setters: 
+    // sets in a matrix position (m row and n column) a given value
+    public void set (int m, int n, double value){
+        Vector vector = this.values[n];
+        vector.set(m, value);
+ 
+    };
+    //sets in a matrix column c a given vector v
     public void setColumn(int c, Vector v) {
-        for(int i=0;i<rows;i++) values[i].set(c,v.get(i));
-    }
+        this.values[c] = v;
+    };
+    //sets in a matrix row r a given vector v
+    public void setRow(int r, Vector v) {
+        for(int i=0;i<this.rows;i++){
+            this.values[i].set(r,v.getPositionValue(i));
+        }
+    };
+    //sets all values in a matrix to zero
     public void zero() {
-        for(Vector v : values) v.zero();
-    }
-    public int rows() {
-        
-    }
-    public int columns() {
-        
-    }
-    public void addColumn(Vector v) {
-        
-    }
+        for(Vector v : this.values) {
+            v.zero();
+        }
+    };
+    
+    //Prints the matrix
+    public void printMatrix(){
+        System.out.println();
+        for(Integer m = 0; m < this.rows; m++){
+            for(Vector v : this.values){  
+                System.out.print(v.getArray()[m] + " ");      
+            }
+            System.out.println();
+        }
+    };
+    
+    // 3D 
+    //Creates a rotation matrix of angle alpha, it also checks the matrix dimensions
+    public void create3DRotationZ(double alpha){
+        if(this.cols == 3 && this.rows ==3){
+            this.values[0].set(0, Math.cos(alpha));
+            this.values[1].set(0, -Math.sin(alpha));
+            this.values[0].set(1, Math.sin(alpha));
+            this.values[1].set(1, Math.cos(alpha));
+            this.values[2].set(2, 1);  
+        }
+        else{
+            System.out.println("Matrix dimension doesn't match");
+        }
+    };
+
+
 }
+    
+    
+    
+
