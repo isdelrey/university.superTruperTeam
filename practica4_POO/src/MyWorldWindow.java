0fe599e45ca87d15170751afd633870bd4ecdbc3
ivosequeros <ivo@MacBook-Pro-de-Ivo-2.local>
2016-12-08 18:03:07 +0100

    Fancyation haha

--- practica4_POO/src/MyWorldWindow.java
+++ practica4_POO/src/MyWorldWindow.java
@@ -1,7 +1,18 @@
 import javax.swing.JOptionPane;
 import game.MyPolygon;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.Graphics;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridBagLayout;
+import java.awt.Window;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.plaf.basic.BasicMenuBarUI;
 
 /*
 ┌──────────────────────────────────────────────────────────────────────────────────┐
@@ -27,9 +38,28 @@ public class MyWorldWindow extends javax.swing.JFrame {
     public MyWorldWindow() {
         initComponents();
         this.setResizable(false);
-        panel1.initWorld(worldWidth,worldHeight,worldNAgents);
-        Dimension d = new Dimension(worldWidth,worldHeight+20);
+        Dimension d = new Dimension(worldWidth,worldHeight+100);
         this.setSize(d);
+        panel1.initWorld(this.getWidth(), this.getHeight() - 100,worldNAgents);
+        jMenuBar1.setLayout(new GridBagLayout());
+        jMenuBar1.setUI ( new BasicMenuBarUI (){
+            public void paint ( Graphics g, JComponent c ){
+               g.setColor ( Color.yellow );
+               g.fillRect ( 0, 0, c.getWidth (), c.getHeight () );
+            }
+        });
+    }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        try {
+            int x = this.getWidth() / 2 - 377;
+            BufferedImage img = ImageIO.read(ClassLoader.getSystemResource("resources/text.png"));
+            jPanel1.getGraphics().drawImage(img, x, 5, null);
+        }
+        catch(IOException e) {
+            System.out.println(e.toString());
+        }
     }
     /**
      * This method is called from within the constructor to initialize the form.
@@ -41,12 +71,14 @@ public class MyWorldWindow extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel6 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         panel1 = new Panel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu3 = new javax.swing.JMenu();
         jMenuItem9 = new javax.swing.JMenuItem();
         jMenuItem10 = new javax.swing.JMenuItem();
         jMenuItem11 = new javax.swing.JMenuItem();
+        jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
@@ -64,6 +96,21 @@ public class MyWorldWindow extends javax.swing.JFrame {
         setFocusTraversalKeysEnabled(false);
         setSize(new java.awt.Dimension(800, 650));
 
+        jPanel1.setBackground(java.awt.Color.yellow);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 906, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.PAGE_START);
+
         panel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         panel1.addComponentListener(new java.awt.event.ComponentAdapter() {
             public void componentResized(java.awt.event.ComponentEvent evt) {
@@ -79,12 +126,26 @@ public class MyWorldWindow extends javax.swing.JFrame {
         );
         panel1Layout.setVerticalGroup(
             panel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 478, Short.MAX_VALUE)
+            .addGap(0, 400, Short.MAX_VALUE)
         );
 
         getContentPane().add(panel1, java.awt.BorderLayout.CENTER);
 
-        jMenu3.setText("Play");
+        jMenuBar1.setBackground(java.awt.Color.red);
+        jMenuBar1.setBorderPainted(false);
+
+        jMenu3.setForeground(new java.awt.Color(204, 0, 0));
+        jMenu3.setText("Play     ");
+        jMenu3.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+        jMenu3.addMenuListener(new javax.swing.event.MenuListener() {
+            public void menuSelected(javax.swing.event.MenuEvent evt) {
+            }
+            public void menuDeselected(javax.swing.event.MenuEvent evt) {
+                jMenu3MenuDeselected(evt);
+            }
+            public void menuCanceled(javax.swing.event.MenuEvent evt) {
+            }
+        });
 
         jMenuItem9.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_1, 0));
         jMenuItem9.setText("Start");
@@ -113,9 +174,29 @@ public class MyWorldWindow extends javax.swing.JFrame {
         });
         jMenu3.add(jMenuItem11);
 
+        jCheckBoxMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0));
+        jCheckBoxMenuItem1.setText("Full Screen");
+        jCheckBoxMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBoxMenuItem1ActionPerformed(evt);
+            }
+        });
+        jMenu3.add(jCheckBoxMenuItem1);
+
         jMenuBar1.add(jMenu3);
 
-        jMenu1.setText("World");
+        jMenu1.setForeground(new java.awt.Color(204, 0, 0));
+        jMenu1.setText("World     ");
+        jMenu1.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+        jMenu1.addMenuListener(new javax.swing.event.MenuListener() {
+            public void menuSelected(javax.swing.event.MenuEvent evt) {
+            }
+            public void menuDeselected(javax.swing.event.MenuEvent evt) {
+                jMenu1MenuDeselected(evt);
+            }
+            public void menuCanceled(javax.swing.event.MenuEvent evt) {
+            }
+        });
 
         jMenuItem5.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, 0));
         jMenuItem5.setText("Reset Settings");
@@ -164,8 +245,20 @@ public class MyWorldWindow extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenu1);
 
+        jMenu2.setForeground(new java.awt.Color(204, 0, 0));
         jMenu2.setText("Animation");
+        jMenu2.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+        jMenu2.addMenuListener(new javax.swing.event.MenuListener() {
+            public void menuSelected(javax.swing.event.MenuEvent evt) {
+            }
+            public void menuDeselected(javax.swing.event.MenuEvent evt) {
+                jMenu2MenuDeselected(evt);
+            }
+            public void menuCanceled(javax.swing.event.MenuEvent evt) {
+            }
+        });
 
+        jMenuItem8.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_R, java.awt.event.InputEvent.CTRL_MASK));
         jMenuItem8.setText("Reset Settings");
         jMenuItem8.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -174,6 +267,7 @@ public class MyWorldWindow extends javax.swing.JFrame {
         });
         jMenu2.add(jMenuItem8);
 
+        jMenuItem6.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
         jMenuItem6.setText("Change Speed");
         jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -182,6 +276,7 @@ public class MyWorldWindow extends javax.swing.JFrame {
         });
         jMenu2.add(jMenuItem6);
 
+        jMenuItem7.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_L, java.awt.event.InputEvent.CTRL_MASK));
         jMenuItem7.setText("Change Latency Rate");
         jMenuItem7.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -200,28 +295,28 @@ public class MyWorldWindow extends javax.swing.JFrame {
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         // TODO add your handling code here:
         worldNAgents = Integer.parseInt(JOptionPane.showInputDialog("Number of Agents:",worldNAgents));
-        panel1.initWorld(worldHeight,worldWidth,worldNAgents);
+        panel1.initWorld(this.getWidth(), this.getHeight() - 100,worldNAgents);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
         // TODO add your handling code here:
-        panel1.initWorld(worldHeight,worldWidth,worldNAgents);
+        panel1.initWorld(this.getWidth(), this.getHeight() - 100,worldNAgents);
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         // TODO add your handling code here:
         worldWidth = Integer.parseInt(JOptionPane.showInputDialog("Width:",worldWidth));
-        panel1.initWorld(worldHeight,worldWidth,worldNAgents);
-        Dimension d = new Dimension(worldWidth,worldHeight+50);
+        Dimension d = new Dimension(worldWidth,worldHeight+100);
         this.setSize(d);
+        panel1.initWorld(this.getWidth(), this.getHeight() - 100,worldNAgents);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         // TODO add your handling code here:
         worldHeight = Integer.parseInt(JOptionPane.showInputDialog("Height:",worldHeight));
-        panel1.initWorld(worldHeight,worldWidth,worldNAgents);
-        Dimension d = new Dimension(worldWidth,worldHeight+50);
+        Dimension d = new Dimension(worldWidth,worldHeight+100);
         this.setSize(d);
+        panel1.initWorld(this.getWidth(), this.getHeight() - 100,worldNAgents);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
@@ -229,9 +324,9 @@ public class MyWorldWindow extends javax.swing.JFrame {
         worldHeight=600;
         worldWidth=800;
         worldNAgents=20;
-        panel1.initWorld(worldHeight,worldWidth,worldNAgents);
-        Dimension d = new Dimension(worldWidth,worldHeight+50);
+        Dimension d = new Dimension(this.getWidth(),this.getHeight()+100);
         this.setSize(d);
+        panel1.initWorld(this.getWidth(), this.getHeight() - 100,worldNAgents);
     }//GEN-LAST:event_jMenuItem5ActionPerformed
 
     private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
@@ -269,6 +364,41 @@ public class MyWorldWindow extends javax.swing.JFrame {
         // TODO add your handling code here:
         panel1.step();
     }//GEN-LAST:event_jMenuItem11ActionPerformed
+
+    private void jMenu2MenuDeselected(javax.swing.event.MenuEvent evt) {//GEN-FIRST:event_jMenu2MenuDeselected
+        // TODO add your handling code here:
+        repaint();
+    }//GEN-LAST:event_jMenu2MenuDeselected
+
+    private void jMenu1MenuDeselected(javax.swing.event.MenuEvent evt) {//GEN-FIRST:event_jMenu1MenuDeselected
+        // TODO add your handling code here:
+        repaint();
+    }//GEN-LAST:event_jMenu1MenuDeselected
+
+    private void jMenu3MenuDeselected(javax.swing.event.MenuEvent evt) {//GEN-FIRST:event_jMenu3MenuDeselected
+        // TODO add your handling code here:
+        repaint();
+    }//GEN-LAST:event_jMenu3MenuDeselected
+
+    private void jCheckBoxMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem1ActionPerformed
+        // TODO add your handling code here:
+        GraphicsDevice d;
+        GraphicsEnvironment e = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        d = e.getDefaultScreenDevice();
+        if(d.getFullScreenWindow() == null) {
+            jMenuItem2.setEnabled(false);
+            jMenuItem3.setEnabled(false);
+            d.setFullScreenWindow(this);
+        }
+        else {
+            jMenuItem2.setEnabled(true);
+            jMenuItem3.setEnabled(true);
+            d.setFullScreenWindow(null);
+        }
+        panel1.initWorld(this.getWidth(), this.getHeight() - 100,worldNAgents);
+        Dimension dim = new Dimension(this.getWidth(),this.getHeight()+100);
+        this.setSize(dim);
+    }//GEN-LAST:event_jCheckBoxMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -311,6 +441,7 @@ public class MyWorldWindow extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
@@ -327,6 +458,7 @@ public class MyWorldWindow extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem7;
     private javax.swing.JMenuItem jMenuItem8;
     private javax.swing.JMenuItem jMenuItem9;
+    private javax.swing.JPanel jPanel1;
     private Panel panel1;
     // End of variables declaration//GEN-END:variables
 }
